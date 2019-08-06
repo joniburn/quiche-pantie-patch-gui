5944eed223ad39233a5f3367800a5ae7e195c8b6
Joniburn <joniburn@gmail.com>
2019-08-06 17:43:17 +0900

    Angular CLIでプロジェクトを作成
    ng new --prefix=qpp --style=scss --skip-git quiche-pantie-patch-gui

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qpp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'quiche-pantie-patch-gui';
+}
