8454388fac7909a80cb91d6bc7de6179ef651e68
joniburn <joniburn@gmail.com>
2019-08-08 01:31:41 +0900

    情報ダイアログを実装

---
+++ src/app/info-dialog/info-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qpp-info-dialog',
+  templateUrl: './info-dialog.component.html',
+  styleUrls: ['./info-dialog.component.scss']
+})
+export class InfoDialogComponent {
+}
