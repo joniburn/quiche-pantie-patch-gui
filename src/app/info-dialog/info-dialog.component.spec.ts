986b952dc5361c7306515075c56228475249eb53
joniburn <joniburn@gmail.com>
2020-12-07 19:24:01 +0900

    ng update @angular/core @angular/cli

--- src/app/info-dialog/info-dialog.component.spec.ts
+++ src/app/info-dialog/info-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InfoDialogComponent } from './info-dialog.component';
 
@@ -6,7 +6,7 @@ describe('InfoDialogComponent', () => {
   let component: InfoDialogComponent;
   let fixture: ComponentFixture<InfoDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InfoDialogComponent ]
     })
--- src/app/zip-dialog/zip-dialog.component.spec.ts
+++ src/app/zip-dialog/zip-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ZipDialogComponent } from './zip-dialog.component';
 
@@ -6,7 +6,7 @@ describe('ZipDialogComponent', () => {
   let component: ZipDialogComponent;
   let fixture: ComponentFixture<ZipDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ZipDialogComponent ]
     })
